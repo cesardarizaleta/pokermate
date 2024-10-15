85eba8f8a8cbc92a8ae4533d34aab06a20c835dd
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-14 21:39:48 -0400

    Modificando PlayBtn

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,4 @@
 extends Control
 
-
-func _on_play_pressed() -> void:
-	# Para Cambiar de scena:
+func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
